0687f741f1f598b9b809662fd59e1fe059b8b467
buenoIago <iagocedup@gmail.com>
2026-09-25 16:52:37 -0300

    adiciona projeto inicial

--- OCRBancario/Program.cs
+++ OCRBancario/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Clear();
-Console.WriteLine("Hello, World!");
+﻿string[][] digitos =
+[
+    [" _ ", "| |", "|_|"], // 0
+    ["   ", "  |", "  |"], // 1
+    [" _ ", " _|", "|_ "], // 2
+    [" _ ", " _|", " _|"], // 3
+    ["   ", "|_|", "  |"], // 4
+    [" _ ", "|_ ", " _|"], // 5
+    [" _ ", "|_ ", "|_|"], // 6
+    [" _ ", "  |", "  |"], // 7
+    [" _ ", "|_|", "|_|"], // 8
+    [" _ ", "|_|", " _|"]  // 9
+];
+
+Console.Write("Digite o número da conta: ");
+string conta = Console.ReadLine()!;
+
+for (int linha = 0; linha < 3; linha++)
+{
+    for (int i = 0; i < conta.Length; i++)
+    {
+        int numero = conta[i] - '0';
+
+        Console.Write(digitos[numero][linha]);
+
+    }
+
+    Console.WriteLine();
+}
